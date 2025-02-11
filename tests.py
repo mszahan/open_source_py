31a0b1ed7aa36bf97ac4ce4acff44e375398dc8f
mszahan <mszahan1@gmail.com>
2025-02-11 18:51:41 +0600

    test mulitple parameter with parametrize test of pytest

--- tests.py
+++ tests.py
@@ -1,11 +1,25 @@
 import reminder as app
 from reminder import Task
+import pytest
 
-def test_find_task():
+
+
+# this test will be like both of the following tests 
+#the test with multiple parameter with pytest
+@pytest.mark.parametrize('test_input, expected',
+                         [
+                             ('buy bread', Task(name='buy bread')),
+                             ('buy milk', None)
+                         ])
+def test_find_task(test_input, expected):
     task_list = [Task(name='pay rent'), Task(name='buy bread')]
-    assert app._find_task('buy bread', task_list) == Task(name='buy bread')
+    assert app._find_task(test_input, task_list) == expected
+
+# def test_find_task():
+#     task_list = [Task(name='pay rent'), Task(name='buy bread')]
+#     assert app._find_task('buy bread', task_list) == Task(name='buy bread')
 
 
-def test_find_none():
-    task_list = [Task(name='pay rent'), Task(name='buy bread')]
-    assert app._find_task('buy milk', task_list) is None
+# def test_find_none():
+#     task_list = [Task(name='pay rent'), Task(name='buy bread')]
+#     assert app._find_task('buy milk', task_list) is None
