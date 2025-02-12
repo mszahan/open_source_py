469436437975b5d435bc88332abe94fb436e693e
mszahan <mszahan1@gmail.com>
2025-02-12 17:16:12 +0600

    installed black and reformatted the test file with black

--- tests.py
+++ tests.py
@@ -4,29 +4,29 @@ import datetime as dt
 import pytest
 
 
-
 def test_to_date():
-    assert app._to_date('2022-09-02') == dt.date(2022, 9, 2)
+    assert app._to_date("2022-09-02") == dt.date(2022, 9, 2)
 
 
 def test_to_date_exception():
     with pytest.raises(ValueError):
-        app._to_date('1132')
+        app._to_date("1132")
+
 
 @pytest.fixture
 def task_list():
-    return [Task(name='pay rent'), Task(name='buy bread')]
+    return [Task(name="pay rent"), Task(name="buy bread")]
 
-# this test will be like both of the following tests 
-#the test with multiple parameter with pytest
-@pytest.mark.parametrize('test_input, expected',
-                         [
-                             ('buy bread', Task(name='buy bread')),
-                             ('buy milk', None)
-                         ])
+
+# this test will be like both of the following tests
+# the test with multiple parameter with pytest
+@pytest.mark.parametrize(
+    "test_input, expected", [("buy bread", Task(name="buy bread")), ("buy milk", None)]
+)
 def test_find_task(test_input, expected, task_list):
     # task_list = [Task(name='pay rent'), Task(name='buy bread')]
     assert app._find_task(test_input, task_list) == expected
+
 
 # def test_find_task():
 #     task_list = [Task(name='pay rent'), Task(name='buy bread')]
@@ -37,7 +37,8 @@ def test_find_task(test_input, expected, task_list):
 #     task_list = [Task(name='pay rent'), Task(name='buy bread')]
 #     assert app._find_task('buy milk', task_list) is None
 
-#function name must start with test_
+
+# function name must start with test_
 def test_save_load_task_list(task_list):
     app._save_task_list(task_list)
     load_list = app._get_task_list()
