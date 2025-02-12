9717755a955b60411efb5185e2753d06522eb50f
mszahan <mszahan1@gmail.com>
2025-02-12 16:51:32 +0600

    used pytest fixture to list out all the task list

--- tests.py
+++ tests.py
@@ -13,6 +13,10 @@ def test_to_date_exception():
     with pytest.raises(ValueError):
         app._to_date('1132')
 
+@pytest.fixture
+def task_list():
+    return [Task(name='pay rent'), Task(name='buy bread')]
+
 # this test will be like both of the following tests 
 #the test with multiple parameter with pytest
 @pytest.mark.parametrize('test_input, expected',
@@ -20,8 +24,8 @@ def test_to_date_exception():
                              ('buy bread', Task(name='buy bread')),
                              ('buy milk', None)
                          ])
-def test_find_task(test_input, expected):
-    task_list = [Task(name='pay rent'), Task(name='buy bread')]
+def test_find_task(test_input, expected, task_list):
+    # task_list = [Task(name='pay rent'), Task(name='buy bread')]
     assert app._find_task(test_input, task_list) == expected
 
 # def test_find_task():
@@ -32,3 +36,9 @@ def test_find_task(test_input, expected):
 # def test_find_none():
 #     task_list = [Task(name='pay rent'), Task(name='buy bread')]
 #     assert app._find_task('buy milk', task_list) is None
+
+#function name must start with test_
+def test_save_load_task_list(task_list):
+    app._save_task_list(task_list)
+    load_list = app._get_task_list()
+    assert task_list == load_list
